f4f11bd6ffa2f2c2d80fb32be8a8843cc5b348c7
kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2025-06-24 21:23:55 +0530

    changes were made into the website

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -39,7 +39,7 @@ export default function PrivacyPolicy() {
               </ul>
             </section>
 
-            {/* Add more sections as needed */}
+            
           </div>
         </motion.div>
       </div>
